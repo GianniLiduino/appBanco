37aeb4dc4f4cac6185289bdb1197dc14ad15214f
GianniLiduino <gianniliduinociuldin@outlook.com>
2022-07-25 13:59:52 -0300

    ID 11

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -75,6 +75,13 @@ const list = [
         date: '10/07/2022',
         type: 1 // receitas / entradas
     },
+    {
+        id: 11,
+        label: 'PIX de Bianca',
+        value: '220,00',
+        date: '10/07/2022',
+        type: 1 // receitas / entradas
+    },
 ]
 
 export default function Home() {
